e5e31f5e9e94b89dbdadacc0ad2d9110c3584dc9
Jorge Nachtigall <jorge.vaz@melhorenvio.com>
2024-05-30 20:24:44 -0300

    fix: package name on setup.py fixed

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="datawizard",
+    name="pandas2redshift",
     version="0.0.1",
     author="Jorge Nachtigall",
     author_email="jlnvjunior@gmail.com",
     description="A tool for exporting Pandas dataframes to Redshift tables",
     packages=find_packages(),
     install_requires=["boto3>=1.34.109", "SQLAlchemy>=1.4.52", "pandas>=2.0.0"],
+    python_requires=">=3.10",
 )
